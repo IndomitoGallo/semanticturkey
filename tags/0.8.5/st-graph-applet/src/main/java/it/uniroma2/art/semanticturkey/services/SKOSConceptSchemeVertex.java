277e25160f5bbaed8296c7876a81fbcd49764f73
manuel.fiorelli@gmail.com <manuel.fiorelli@gmail.com@e23e5b5a-e442-0410-a6a2-63c1b3e16cff>
2012-05-22 19:51:58 +0000

    Tagging the 0.8.5 release

---
+++ tags/0.8.5/st-graph-applet/src/main/java/it/uniroma2/art/semanticturkey/services/SKOSConceptSchemeVertex.java
@@ -0,0 +1,9 @@
+package it.uniroma2.art.semanticturkey.services;
+
+public class SKOSConceptSchemeVertex extends SKOSVertex {
+
+	public SKOSConceptSchemeVertex(String name) {
+		super(name, name);
+	}
+	
+}
