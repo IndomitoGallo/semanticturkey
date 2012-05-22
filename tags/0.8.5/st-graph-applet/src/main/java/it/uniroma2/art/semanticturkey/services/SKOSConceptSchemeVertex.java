aa3a14865069a23c64f9cf8a5cf22c51f2249e76
manuel.fiorelli@gmail.com <manuel.fiorelli@gmail.com@e23e5b5a-e442-0410-a6a2-63c1b3e16cff>
2012-05-22 20:18:22 +0000

    Removed not building release 0.8.5.

--- tags/0.8.5/st-graph-applet/src/main/java/it/uniroma2/art/semanticturkey/services/SKOSConceptSchemeVertex.java
+++
@@ -1,9 +0,0 @@
-package it.uniroma2.art.semanticturkey.services;
-
-public class SKOSConceptSchemeVertex extends SKOSVertex {
-
-	public SKOSConceptSchemeVertex(String name) {
-		super(name, name);
-	}
-	
-}
--- tags/0.8.5/st-graph-applet/src/main/java/it/uniroma2/art/semanticturkey/services/SKOSXLServiceClient.java
+++
@@ -1,9 +0,0 @@
-package it.uniroma2.art.semanticturkey.services;
-
-public class SKOSXLServiceClient extends SKOSServiceClient {
-	
-	public SKOSXLServiceClient(boolean humanReadable, String lang) {
-		super(humanReadable, lang);
-	}
-	
-}
